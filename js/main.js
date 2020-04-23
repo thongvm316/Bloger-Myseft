61185181b5ccac408255a0a3efba3d87ad72360b
Minh Thong <vominhthong1603@gmail.com>
2020-04-23 16:20:53 +0700

    Fn Nav & Site-title

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const navMenu = document.querySelector('.nav-menu');
+const toggleCollapse = document.querySelector('.toggle-collapse');
+
+toggleCollapse.addEventListener('click', () => {
+    navMenu.classList.toggle('collapse')
+})
